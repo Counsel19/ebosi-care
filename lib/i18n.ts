e24809cd757e450686306d82e6e0871f1fd54931
CounselJS <okpabicounsel@gmail.com>
2025-04-28 16:11:46 +0100

    fixing build

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,34 +1,32 @@
-"use client"
+"use client";
 
 // lib/i18n.ts
-import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import enCommon from '@/public/locales/en/common.json';
-import enHome from '@/public/locales/en/home.json';
-import enAbout from '@/public/locales/en/about.json';
-import enForm from '@/public/locales/en/form.json';
+import enCommon from "@/public/locales/en/common.json";
+import enHome from "@/public/locales/en/home.json";
+import enAbout from "@/public/locales/en/about.json";
+import enForm from "@/public/locales/en/form.json";
 
-import esCommon from '@/public/locales/es/common.json';
-import esHome from '@/public/locales/es/home.json';
-import esAbout from '@/public/locales/es/about.json';
-import esForm from '@/public/locales/es/form.json';
+import esCommon from "@/public/locales/es/common.json";
+import esHome from "@/public/locales/es/home.json";
+import esAbout from "@/public/locales/es/about.json";
+import esForm from "@/public/locales/es/form.json";
 
 // Define the translations
 const resources = {
   en: { common: enCommon, about: enAbout, home: enHome, form: enForm },
-  es: { common: esCommon, about: esAbout, home: esHome, form: esForm  },
+  es: { common: esCommon, about: esAbout, home: esHome, form: esForm },
 };
 
-i18next
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng:  localStorage.getItem("lang") || "en", 
-    fallbackLng: "en", 
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18next.use(initReactI18next).init({
+  resources,
+  lng: (typeof window !== "undefined" && localStorage.getItem("lang")) || "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-  export default i18next
+export default i18next;
